52b9187578586761198d234d7b7d2baa38c14164
Caley Finn <caleyreuben@gmail.com>
2018-01-02 16:44:11 +1100

    Tests for boundary transitions

--- src/test/java/latticesim/TransitionsTest.java
+++ src/test/java/latticesim/TransitionsTest.java
@@ -43,10 +43,51 @@ public class TransitionsTest {
     }
 
     @Test
-    public void asepDoTransition() throws Exception {
-        SimOptions options = loadTestOptions("asep-test-options.json");
-        String [] expectedTransitions = loadExpectedOutput(
-            "asep-test-out2.json");
+    public void asepDoBulkTransition() throws Exception {
+        // Do this transition
+        String thisTransition = "{\"rate\":1.0,\"c\":1,\"position\":8,\"window\":2,\"ispec\":1,\"fromC\":2}";
+
+
+        doTransition(
+            "asep-test-options.json",
+            thisTransition,
+            "asep-test-out2.json"
+        );
+    }
+
+    @Test
+    public void asepDoLeftBoundaryTransition() throws Exception {
+        // Do this transition
+        String thisTransition = "{\"rate\":0.29,\"c\":1,\"position\":0,\"window\":1,\"ispec\":0,\"fromC\":0}";
+
+
+        doTransition(
+            "asep-test-options.json",
+            thisTransition,
+            "asep-test-out3.json"
+        );
+    }
+
+    @Test
+    public void asepDoRightBoundaryTransition() throws Exception {
+        // Do this transition
+        String thisTransition = "{\"rate\":0.22,\"c\":0,\"position\":14,\"window\":1,\"ispec\":2,\"fromC\":1}";
+
+        doTransition(
+            "asep-test-options.json",
+            thisTransition,
+            "asep-test-out4.json"
+        );
+    }
+
+    private void doTransition(
+        String optionsFile,
+        String transition,
+        String outputFile
+    ) throws Exception{
+        SimOptions options = loadTestOptions(optionsFile);
+
+        String [] expectedTransitions = loadExpectedOutput(outputFile);
         Arrays.sort(expectedTransitions);
 
         LatticeConfiguration config = new LatticeConfiguration(
@@ -55,10 +96,8 @@ public class TransitionsTest {
 
         Transitions transitions = new Transitions(config, options.transitions);
 
-        // Do this transition
-        String thisTransition = "{\"rate\":1.0,\"c\":1,\"position\":8,\"window\":2,\"ispec\":1,\"fromC\":2}";
-
-        transitions.forceTransition(thisTransition);
+        // Force the transition
+        transitions.forceTransition(transition);
 
         String [] outputTransitions = transitions.transitionsJSON();
         Arrays.sort(outputTransitions);
