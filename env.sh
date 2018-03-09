e52b57ae0d693260d54872a8b2ab1d45ed69eb7c
Caley Finn <caleyreuben@gmail.com>
2018-03-09 12:35:47 +1100

    Removed old files

--- env.sh
+++
@@ -1,4 +0,0 @@
-#
-# Anything common between the scripts
-
-CLASSPATH=./target/classes:lib/gson-2.8.2.jar:lib/commons-math3-3.6.1.jar
--- run.sh
+++
@@ -1,5 +0,0 @@
-OPENEPSIM_DIR=`dirname $0`
-
-CLASSPATH="$OPENEPSIM_DIR/target/classes:$OPENEPSIM_DIR/lib/gson-2.8.2.jar:$OPENEPSIM_DIR/lib/commons-math3-3.6.1.jar"
-
-java -classpath "$CLASSPATH" openepsim.OpenEPSimulation
